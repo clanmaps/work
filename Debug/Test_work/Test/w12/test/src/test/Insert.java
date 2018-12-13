3bfed3954f55a0e345d5d9654e26f063f4b1a5e5
wangjixin <986766483@qq.com>
2018-12-13 19:14:49 +0800

    update

--- Debug/Test_work/Test/w12/test/src/test/Insert.java
+++ Debug/Test_work/Test/w12/test/src/test/Insert.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 
-public class Insert {
+public class statis {
 	
 	public static  Connection getConn() {  //ms1连接
 	    String driver = "com.mysql.jdbc.Driver";
@@ -27,32 +27,20 @@ public class Insert {
 		Connection conn = conns;
 	    String sql;
 		Statement stmt = conn.createStatement();
-	    sql = "select * from test";
+	    sql = "select count(1) from test";
 	    ResultSet rs = stmt.executeQuery(sql);
 	    System.out.println("===================");
 	    System.out.println("ID\t姓名");
 	    while (rs.next()) {
-	    	System.out.println(rs.getInt(1)+"\t"+rs.getString(2));
+	    	System.out.println(rs.getInt(1)+"\t");
 	    }
 	    System.out.println("===================");
 	    return 0;
 	}
 	
 	public static void main(String args[]) throws Exception {
+		Connection conn = statis.getConn(); //ms1写
 
-		for(int idx = 1,idx2 = 11,idx3 = 21;idx <= 10;++idx,++idx2,++idx3) {
-		Connection conn = Insert.getConn(); //ms1写
-	    String newname = "zhang";
-	    String sql = "insert into t1 (c1,c2,c3,c4,c5) values(?,?,?,?,?)"; //插入操作
-	    PreparedStatement pstmt ;
-	    pstmt = (PreparedStatement) conn.prepareStatement(sql);
-        pstmt.setInt(1, idx);
-        pstmt.setInt(2, idx2);
-        pstmt.setInt(3, idx3);
-        pstmt.setInt(4, idx);
-        pstmt.setString(5, newname);
-	    pstmt.executeUpdate();
-        pstmt.close();
         conn.close();
 		}
 		//System.out.println("The result of Insert:");
